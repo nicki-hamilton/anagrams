3857a2c8be19abebb67fa84c5dd484573f84bbae
niseekah <niseekahhamilton@mec.science>
2020-01-15 16:00:45 -0500

    1/15/20 change

--- app/src/main/java/com/google/engedu/anagrams/AnagramDictionary.java
+++ app/src/main/java/com/google/engedu/anagrams/AnagramDictionary.java
@@ -21,6 +21,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 
@@ -31,39 +34,86 @@ public class AnagramDictionary {
     private static final int DEFAULT_WORD_LENGTH = 3;
     private static final int MAX_WORD_LENGTH = 7;
     private Random random = new Random();
+    ArrayList<String> wordList = new ArrayList<String>();
+    HashMap<String, ArrayList<String>> lettersToWord= new HashMap<String, ArrayList<String>>();
+    HashSet<String> wordSet = new HashSet<String>();
 
-    public AnagramDictionary(Reader reader) throws IOException {
+
+    public AnagramDictionary(Reader reader) throws IOException
+    {
         BufferedReader in = new BufferedReader(reader);
         String line;
-        ArrayList<String> wordlist = new ArrayList<String>();
         while((line = in.readLine()) != null) {
             String word = line.trim();
-            wordlist.add(word);
+            wordList.add(word);
+            wordSet.add(word);
+
+            // get signature for word
+            String key = sortLetters(word);
+
+            //lookup key on table
+            if(lettersToWord.containsKey(key))
+            {
+                //key is on table, add word to the List for that key
+                ArrayList<String>listofWords = lettersToWord.get(key);
+                listofWords.add(word);
+            }
+            else
+            {
+                ArrayList<String> temp = new ArrayList<String>();
+                temp.add(word);
+                lettersToWord.put(key,temp);
+            }
             Log.d(word, "apple");
         }
 
     }
-
-    public boolean isGoodWord(String word, String base) {
-        return true;
+    // Function called (sortLetters) that takes a String and returns another
+    // string with the same letters in alphabetical order.
+    public String sortLetters(String inputword)
+    {
+        char letters[] = inputword.toCharArray();
+        Arrays.sort(letters);
+        String signature = new String(letters);
+        return signature;
     }
 
-    public List<String> getAnagrams(String targetWord) {
-        int temp;
-        ArrayList<String> result = new ArrayList<String>();
-        String sort = targetWord;
-        char[] ch  = new char[sort.length()];
-        for(int i = 0; i < sort.length(); i++)
+    // Check work tomorrow!
+    public boolean isGoodWord(String word, String base)
+    {
+
+        if(wordSet.contains(word))
         {
-            temp = ch[i];
-            ch[i] = ch[i+1];
-            ch[i+1] = (char) temp;
-
+            if(!word.contains(base))
+            {
+                return true;
+            }
         }
+        return false;
+    }
 
+    public List<String> getAnagrams(String targetWord)
+    {
+        ArrayList<String> result = new ArrayList<String>();
+        // Create signature for the input word
+        String targetWordSignature = sortLetters(targetWord.toLowerCase());
 
+        //For each word in the dictionary
+        for(String word: wordList)
+        {
+            //get signature of the word
+            String signatureWord = sortLetters(word);
+
+            //compare signature (input word and dictionary word)
+            if(targetWordSignature.equals(signatureWord))
+            {
+                result.add(word);
+            }
+        }
         return result;
     }
+
+
 
     public List<String> getAnagramsWithOneMoreLetter(String word) {
         ArrayList<String> result = new ArrayList<String>();
